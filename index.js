e522a23205198b834f4c00b8e99b09dc4d2a38e4
kobrak1 <aburakkarhan@gmail.com>
2025-02-28 21:24:47 +0300

    8.6: Adding a book

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(name: String, genre: String): [Book!]!
     allAuthors: [Author]!
   }
+
+  type Mutation {
+    addBook(
+      title: String,
+      author: String,
+      published: Int,
+      genres: [String]
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -130,6 +140,14 @@ const resolvers = {
       ...author,
       bookCount: books.filter(book => book.author === author.name).length
     }))
+  },
+
+  Mutation: {
+    addBook: (_, args) => {
+      const book = { ...args, id: uuid };
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
