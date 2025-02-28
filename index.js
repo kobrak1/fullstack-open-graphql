9d4d33aa42d036dfb151b760b8beb939d32e73c6
kobrak1 <aburakkarhan@gmail.com>
2025-02-28 15:09:56 +0300

    8.3: All authors

--- index.js
+++ index.js
@@ -106,10 +106,17 @@ const typeDefs = `
     genres: [String]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book]!
+    allAuthors: [Author]!
   }
 `
 
@@ -118,6 +125,7 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors
   }
 }
 
